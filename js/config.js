4b0c84984cfa16a9a585b3d190eb69b4f0f401db
Danuzu11 <Danuzu11@users.noreply.github.com>
2026-06-21 02:03:01 +0000

    deploy: 32ac58444db2c190738af44176e529397fc00c5f

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Generado automáticamente — no editar manualmente
+window.APP_CONFIG = {
+  apiKey: "AIzaSyDLRx6fiEx7khiuW9nEmole5y0KOvjLipM",
+  clientId: "233076351399-64alcam4doakbo2s51oofcpgednc4efp.apps.googleusercontent.com",
+  sheetId: "1IuUw-C4nx9daHfBFLzArKIKhd0UmFzQr61Djj5Pu0rU"
+};
